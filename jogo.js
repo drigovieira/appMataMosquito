7be467b24955b7972e61e513e163dc495fd84931
Rodrigo Vieira <rodrigopiresvieira@outlook.com>
2022-04-22 00:24:17 -0300

    Troca de lado dos mosquitos

--- jogo.js
+++ jogo.js
@@ -5,7 +5,6 @@ var largura = 0
 function ajustaTamanhoPalcoJogo() {
   altura = window.innerHeight
   largura = window.innerWidth
-  console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
@@ -17,30 +16,36 @@ function posicaoRandomica() {
 
   posicaoX = posicaoX < 0 ? 0 : posicaoX
   posicaoY = posicaoY < 0 ? 0 : posicaoY
-
-  console.log(posicaoX, posicaoY)
   
   // Criar o elemento HTML
   var mosquito = document.createElement('img')
   mosquito.src = 'imagens/mosquito.png'
-  mosquito.className = tamanhoAleatorio()
+  mosquito.className = tamanhoAleatorio() + ladoAleatorio()
   mosquito.style.left = posicaoX + 'px'
   mosquito.style.top = posicaoY + 'px'
   mosquito.style.position = 'absolute'
   
   document.body.appendChild(mosquito)
-
-  
 }
 
 function tamanhoAleatorio() {
   var classe = Math.floor(Math.random() * 3)
   switch(classe) {
     case 0:
-      return 'mosquito1'
+      return 'mosquito1 '
     case 1:
-      return 'mosquito2'
+      return 'mosquito2 '
     case 2:
-      return 'mosquito3'
+      return 'mosquito3 '
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2)
+  switch(classe) {
+    case 0:
+      return 'ladoA '
+    case 1:
+      return 'ladoB '
   }
 }
